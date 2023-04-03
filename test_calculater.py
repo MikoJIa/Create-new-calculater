e39d851d633d017ba17efb9ce36ea5d837e73c24
MikoJIa <Kolya_popov_86@tut.by>
2023-04-03 23:01:04 +0300

    add test_calc_minus and sign

--- test_calculater.py
+++ test_calculater.py
@@ -4,12 +4,26 @@ from calculater import calc_plus, calc_minus, calc_division, calc_multiply, \
     calc_exponent
 
 @pytest.mark.parametrize(
-    'num1, num2, result',
+    'num1, num2, sign, result',
     [
-        (2, 2, 4),
-        (2, -2, 0),
-        (2, 0, 2)
+        (2, 2, '+', 4),
+        (2, -2, '+', 0),
+        (2, 0, '+', 2)
     ]
 )
-def test_calc_plus(num1, num2, result):
+def test_calc_plus(num1, num2, sign, result):
     assert calc_plus(num1, num2) == result
+
+
+@pytest.mark.parametrize(
+    'num1, num2, sign, result',
+    [
+        (2, 1, '-', 1),
+        (3, 2, '-', 1),
+        (2, 0, '-', 2)
+    ]
+)
+def test_calc_minus(num1, num2, sign, result):
+    assert calc_minus(num1, num2) == result
+
+
