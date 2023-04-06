1a16ed0e128acca166b5e6db406803cb050f1961
MikoJIa <Kolya_popov_86@tut.by>
2023-04-06 19:58:05 +0300

    add test_calc_division, test_Zero_Division_Error, test_calc_multiply, test_calc_exponent

--- test_calculater.py
+++ test_calculater.py
@@ -27,3 +27,45 @@ def test_calc_minus(num1, num2, sign, result):
     assert calc_minus(num1, num2) == result
 
 
+@pytest.mark.parametrize(
+    'num1, num2, sign, result',
+    [
+        (10, 2, '/', 5),
+        (10, 5, '/', 2),
+        (5, 2, '/', 2.5)
+    ]
+)
+def test_calc_division(num1, num2, sign, result):
+    assert calc_division(num1, num2) == result
+
+@pytest.mark.parametrize(
+    'num1, num2, sign, result',
+    [
+        (10, 0, '/', ZeroDivisionError)
+    ]
+)
+def test_Zero_Division_Error(num1, num2, sign, result):
+    with pytest.raises(result):
+        assert calc_division(num1, num2) == result
+
+@pytest.mark.parametrize(
+    'num1, num2, sign, result',
+    [
+        (10, 2, '*', 20),
+        (10, -2, '*', -20),
+        (-10, -2, '*', 20)
+    ]
+)
+def test_calc_multiply(num1, num2, sign, result):
+    assert calc_multiply(num1, num2) == result
+
+@pytest.mark.parametrize(
+    'num1, num2, sign, result',
+    [
+        (2, 2, '**', 4),
+        (2, -2, '**', 0.25),
+        (-2, 2, '**', 4)
+    ]
+)
+def test_calc_exponent(num1, num2, sign, result):
+    assert calc_exponent(num1, num2) == result
