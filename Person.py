002e4d248b3395a24def2263135d6943946eeb56
MikoJIa <Kolya_popov_86@tut.by>
2023-05-07 11:57:08 +0300

    add file Person, correct test ZeroDivisionError, calculater division, init,

---
+++ Person.py
@@ -0,0 +1,13 @@
+"property - изучить"
+"обстракт.классы - что это"
+"мексины - что это"
+class Person:
+
+    def __init__(self, name, surname, age, weight, height, hair_color, skin_color):
+        self.name = name
+        self.surname = surname
+        self.age = age
+        self.weight = weight
+        self.height = height
+        self.hair_color = hair_color
+        self.skin_color = skin_color
