56b19421c8a9e85dcb5226501bb21da229b7fe90
MikoJIa <Kolya_popov_86@tut.by>
2023-05-28 20:01:44 +0300

    change class Person

--- Person.py
+++ Person.py
@@ -19,83 +19,40 @@ class Person:
         return f"Person {self.name} - white man"
 
     def set_name(self, value):
-        setattr(self, "name", value)
+        self.name = value
 
     def set_surname(self, value):
-        setattr(self, "surname", value)
+        self.surname = value
 
     def set_age(self, value):
-        setattr(self, "age", value)
+        self.age = value
 
     def set_weight(self, value):
-        setattr(self, "weight", value)
+        self.weight = value
 
     def set_height(self, value):
-        setattr(self, "height", value)
+        self.height = value
 
     def set_hair_color(self, value):
-        setattr(self, "hair_color", value)
+        self.hair_color = value
 
     def set_skin_color(self, value):
-        setattr(self, "skin_color", value)
-
-    def get_print_weight_height_age(self, obj):
-        if not self.__gt__(obj):
-            print(f'{obj.name} весит больше чем {self.name}')
-        else:
-            print(f'{self.name} весит больше чем {obj.name}')
-        if not self.__gt__(obj):
-            print(f'{self.name} выше чем {obj.name}')
-        else:
-            print(f'{obj.name} выше чем {self.name}')
-
-    def compare_by_weight(self, obj: int) -> int:
-        if not isinstance(obj, (int, Person)):
-            raise TypeError('Операнды справа должен иметь тип int или Person')
-        return obj if isinstance(obj, int) else obj.weight
-
-    def compare_by_height(self, obj):
-        if not isinstance(obj, (int, Person)):
-            raise TypeError('Операнды справа должен иметь тип int или Person')
-        return obj if isinstance(obj, int) else obj.height
-
-    def compare_by_age(self, obj):
-        if not isinstance(obj, (int, Person)):
-            raise TypeError('Операнды должны быть типа int или Person')
-        return obj if isinstance(obj, int) else obj.age
+        self.skin_color = value
 
     def __eq__(self, obj):
-        if self.weight:
-            res = self.compare_by_weight(obj)
-            return self.weight == res
-        if self.height:
-            res = self.compare_by_height(obj)
-            return self.height == res
-        if self.age:
-            res = self.compare_by_age(obj)
-            return self.age == res
+        if isinstance(obj, (tuple, Person)):
+            return all((self.weight == obj.weight, self.height == obj.height, self.age == obj.age))
+        raise TypeError('Операнды справа должен иметь тип  Person')
 
     def __lt__(self, obj):
-        if self.weight:
-            res = self.compare_by_weight(obj)
-            return self.weight < res
-        if self.height:
-            res = self.compare_by_height(obj)
-            return self.height < res
-        if self.age:
-            res = self.compare_by_age(obj)
-            return self.age < res
+        if isinstance(obj, Person):
+            return all((self.weight < obj.weight, self.height < obj.height, self.age < obj.age))
+        raise TypeError('Операнды справа должен иметь тип  Person')
 
     def __gt__(self, obj):
-        if self.weight:
-            res = self.compare_by_weight(obj)
-            return self.weight > res
-        if self.height:
-            res = self.compare_by_height(obj)
-            return self.height > res
-        if self.age:
-            res = self.compare_by_age(obj)
-            return self.age > res
+        if isinstance(obj, Person):
+            return all((self.weight > obj.weight, self.height > obj.height, self.age > obj.age))
+        raise TypeError('Операнды справа должен иметь тип  Person')
 
     def person_info(self):
         print(
@@ -105,49 +62,21 @@ class Person:
         )
 
 
-person1 = Person("Mikolai", "Popov", 36, 89, 1.90, "silver", "white")
-person1.person_info()
-person1.height = 1.93
-person2 = Person("Grisha", "Vinokur", 40, 90, 1.83, "black", "black")
-person3 = Person('Volodya', 'Pupkin', 34, 82, 1.85, 'silver', 'white')
-person4 = Person('Petya', 'Petrov', 45, 80, 1.86, 'black', 'white')
-person5 = Person('Sasha', 'Shushkevich', 40, 90, 1.87, 'yellow', 'black')
-print(person2.weight > person1.weight)
-print(person5.height == person4.height)
-print(person1.height < person3.height)
-Person.get_print_weight_height_age(person1, person2)
-print(person5.age < person4.age)
-print(person5.age > person4.age)
+# person1 = Person("Mikolai", "Popov", 36, 89, 1.90, "silver", "white")
+# person1.person_info()
+# person1.height = 1.93
+# person2 = Person("Grisha", "Vinokur", 40, 90, 1.83, "black", "black")
+# person3 = Person('Volodya', 'Pupkin', 34, 82, 1.85, 'silver', 'white')
+# person4 = Person('Petya', 'Petrov', 40, 91, 1.87, 'black', 'white')
+# person5 = Person('Sasha', 'Shushkevich', 40, 90, 1.87, 'yellow', 'black')
+#
+# print(person5 == (40, 90, 1.90))
+# передача через кортеж
+# print(person5 == 90)
+# print(person3 < person2)
+# print(person5 > person4)
+# Person.get_print_weight_height_age(person1, person2)
+# Person.__eq__()
 
-# class BankAccount:
-#
-#     def __init__(self, name, balance):
-#         self.name = name
-#         self.__balance = balance
-#
-#     def get_balance(self):
-#         print('get balance')
-#         return self.__balance
-#
-#     def set_balance(self, value):
-#         if not isinstance(value, int | float):
-#             raise ValueError('Водимое значение должно быть числом!!')
-#         self.__balance = value
-#         print('set balance')
-#
-#     def del_balance(self):
-#         print('del balance')
-#         del self.__balance
-#
-#     balance = property(fget=get_balance, fset=set_balance, fdel=del_balance)
-#
-#
-# user1 = BankAccount('Tanya', 1000)
-# print(user1.balance)
-# user1.balance = 777
-# print(user1.balance)
-# user1.del_balance()
-# user1.balance = 786
-# print(user1.balance)
 
 
--- testPerson.py
+++ testPerson.py
@@ -1,10 +1,11 @@
 from Person import Person
 import pytest
 
-@pytest.mark.parametrize('name, surname, age, weight, height, hair_color, skin_color', [
-    ('Bob', 'Willson', 40, 90, 1.80, 'black', 'white')
-])
-def test__init__(name, surname, age, weight, height, hair_color, skin_color):
+# @pytest.mark.parametrize('name, surname, age, weight, height, hair_color, skin_color', [
+#     ('Bob', 'Willson', 40, 90, 1.80, 'black', 'white')
+# ])
+# def test__init__(name, surname, age, weight, height, hair_color, skin_color):
+def test__init__():
         '''Проверяем полные данные о человеке'''
         person = Person('Bob', 'Willson', 40, 90, 1.80, 'black', 'white')
         assert person.name == 'Bob'
@@ -16,8 +17,10 @@ def test__init__(name, surname, age, weight, height, hair_color, skin_color):
         assert person.skin_color == 'white'
 
 
+person1 = Person("Mikolai", "Popov", 40, 90, 1.90, "silver", "white")
+person2 = Person("Grisha", "Vinokur", 40, 90, 1.90, "black", "black")
+
+
 def test_compare_by_age():
     '''Сравниваем два экземпляра'''
-    person1 = Person("Mikolai", "Popov", 40, 89, 1.90, "silver", "white")
-    person2 = Person("Grisha", "Vinokur", 40, 90, 1.83, "black", "black")
-    assert person1.age == person2.age
+    assert person1 == person2
